19c841de89f0abdb887c384d2df942c9a1634bb8
cariocaphil <cariocaphil@gmail.com>
2021-04-17 13:14:40 +0200

    add map component with user position marker

---
+++ src/components/map.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { API_KEY } from '../utils/constants';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function MapComponent() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: API_KEY
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(async position => {
+      let lat = await position.coords.latitude;
+      let lng = await position.coords.longitude;
+      setLocation({
+        ...location, lat, lng
+      })
+    })
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, [])
+
+  const [location, setLocation] = useState(null);
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>
+  }
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={location ? location : center}
+      zoom={40}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      { /* Child components, such as markers, info windows, etc. */}
+      <Marker position={location} />
+    </GoogleMap>
+  ) : <>loading</>
+}
+
+export default React.memo(MapComponent)
