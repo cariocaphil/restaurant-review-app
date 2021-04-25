6f1728231f93f2639c4f98d164adfcda3e5ddf83
cariocaphil <cariocaphil@gmail.com>
2021-04-25 13:52:14 +0200

    add i18n, show filtered restos if within bounds, streetView infoWindow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantList from "./components/restaurant-list";
 import Map from "./components/map";
 import { data } from "./data/restaurants";
+import { getAverageRating } from "./utils/helper-functions";
 
 function App() {
+  const [currentBounds, setCurrentBounds] = useState(null); // map bounds state
+  const [minRating, setMinRating] = useState(1);
+  const [maxRating, setMaxRating] = useState(5);
+  const [selected, setSelected] = useState(null);
+
+  // handlers for rating form filter
+  const handleMinRating = (e) => {
+    const value = e.target.value;
+    setMinRating(value);
+  };
+
+  const handleMaxRating = (e) => {
+    const value = e.target.value;
+    setMaxRating(value);
+  };
+
+  // handlers for restaurant list
+  const handleDetailsView = (index) => {
+    setSelected(index);
+  };
+
+  const handleClose = () => {
+    setSelected(null);
+  };
+
+  const handleBounds = (bounds) => {
+    setCurrentBounds(bounds);
+  };
+
+  let filteredData = data.filter((restaurant) => {
+    const averageRating = getAverageRating(restaurant.ratings);
+    return averageRating >= minRating && averageRating <= maxRating;
+  });
+
   return (
     <>
-      <RestaurantList data={data} />
-      <Map data={data} />
+      <RestaurantList
+        data={filteredData}
+        minRating={minRating}
+        maxRating={maxRating}
+        handleMaxRating={handleMaxRating}
+        handleMinRating={handleMinRating}
+        currentBounds={currentBounds}
+        handleBounds={handleBounds}
+        selected={selected}
+        handleDetailsView={handleDetailsView}
+        handleClose={handleClose}
+      />
+      <Map data={filteredData} handleBounds={handleBounds} />
     </>
   );
 }
--- src/components/restaurant-list.js
+++ src/components/restaurant-list.js
@@ -1,14 +1,74 @@
 import React from "react";
+import RestaurantItem from "./restaurant-item";
+import { useTranslation } from "react-i18next";
+import { getAverageRating } from "../utils/helper-functions";
 
-function RestaurantList({ data }) {
+function RestaurantList({
+  data,
+  minRating,
+  maxRating,
+  handleMaxRating,
+  handleMinRating,
+  currentBounds,
+  isOpen,
+  selected,
+  handleDetailsView,
+  handleClose,
+}) {
+  const { t } = useTranslation();
+
   return (
-    <ul>
-      {data.map((restaurant, index) => (
-        <li key={index}>
-          <h2>{restaurant.restaurantName}</h2>
-        </li>
-      ))}
-    </ul>
+    <>
+      <form>
+        <label>
+          {t("restaurantList.selectLabelMin")}
+          <select value={minRating} onChange={handleMinRating}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+
+        <div>
+          <label>
+            {t("restaurantList.selectLabelMax")}
+            <select value={maxRating} onChange={handleMaxRating}>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </label>
+        </div>
+      </form>
+      <h2>{t("restaurantList.headline")}</h2>
+      <ul>
+        {data.map((restaurant, index) => (
+          <RestaurantItem
+            selected={selected === index}
+            restaurant={restaurant}
+            isOpen={isOpen}
+            index={index}
+            handleDetailsView={() => handleDetailsView(index)}
+            handleClose={() => handleClose()}
+            currentBounds={currentBounds}
+            isInCurrentMapRange={
+              currentBounds &&
+              currentBounds.contains({
+                lat: restaurant.lat,
+                lng: restaurant.long,
+              })
+            }
+            averageRating={getAverageRating(restaurant.ratings)}
+          />
+        ))}
+      </ul>
+    </>
   );
 }
 
