9fb7efd1532f0b300dacc5a461f74bbb74b1f065
cariocaphil <cariocaphil@gmail.com>
2021-05-01 19:28:05 +0200

    add styling rating form

--- src/components/restaurant-list.js
+++ src/components/restaurant-list.js
@@ -2,8 +2,7 @@ import React from "react";
 import RestaurantItem from "./restaurant-item";
 import { useTranslation } from "react-i18next";
 import { getAverageRating } from "../utils/helper-functions";
-
-import { Select } from "antd";
+import { Select, Form, Input } from "antd";
 const { Option } = Select;
 
 import "./restaurant-list.css";
@@ -24,28 +23,28 @@ function RestaurantList({
 
   return (
     <>
-      <form>
-        <label>
-          {t("restaurantList.selectLabelMin")}
-          <Select
-            style={{ width: 120 }}
-            value={minRating}
-            onChange={(value) => handleMinRating(value)}
-          >
-            <Option value="0">0</Option>
-            <Option value="1">1</Option>
-            <Option value="2">2</Option>
-            <Option value="3">3</Option>
-            <Option value="4">4</Option>
-            <Option value="5">5</Option>
-          </Select>
-        </label>
-
-        <div>
-          <label>
-            {t("restaurantList.selectLabelMax")}
+      <Form layout="inline" style={{ margin: 10 }}>
+        <Input.Group compact>
+          <Form.Item label={t("restaurantList.selectLabelMin")}>
             <Select
-              style={{ width: 120 }}
+              style={{ width: 55 }}
+              value={minRating}
+              onChange={(value) => handleMinRating(value)}
+            >
+              <Option value="0">0</Option>
+              <Option value="1">1</Option>
+              <Option value="2">2</Option>
+              <Option value="3">3</Option>
+              <Option value="4">4</Option>
+              <Option value="5">5</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label={t("restaurantList.selectLabelMax")}
+            style={{ marginLeft: "10px" }}
+          >
+            <Select
+              style={{ width: 55 }}
               value={maxRating}
               onChange={(value) => handleMaxRating(value)}
             >
@@ -56,9 +55,9 @@ function RestaurantList({
               <Option value="4">4</Option>
               <Option value="5">5</Option>
             </Select>
-          </label>
-        </div>
-      </form>
+          </Form.Item>
+        </Input.Group>
+      </Form>
       <h2>{t("restaurantList.headline")}</h2>
       <ul>
         {data.map((restaurant, index) => (
